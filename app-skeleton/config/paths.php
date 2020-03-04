a4f19601118725e353ca0b7a640e09d7d9a60a8b
Sander Tuinstra <sandert2001@hotmail.com>
2020-03-04 15:49:09 +0100

    New paths

--- app-skeleton/config/paths.php
+++ app-skeleton/config/paths.php
@@ -8,13 +8,18 @@ define('DS', DIRECTORY_SEPARATOR);
 
 // \app directory
 define('ROOT', dirname(__DIR__, 1));
+define('BASE_URL', str_replace('index.php', '', $_SERVER['PHP_SELF']));
 
 // App
 define('CONFIG',      ROOT . '/config');
 define('TMP',         ROOT . '/tmp');
 define('SRC',         ROOT . '/src');
+define('ASSETS',      BASE_URL);
 define('CONTROLLERS', SRC  . '/Controllers');
+define('MODELS',      SRC  . '/Models');
+define('ENTITIES',    SRC  . '/Models/Entities');
 define('VIEWS',       SRC  . '/Views');
+define('APP',         CONTROLLERS);
 
 // Fryske Orangjekoeke
 define('FRYSKE_ORANJEKOEKE', ROOT . '/vendor/sandert2001/fryske-oranjekoeke/src');
