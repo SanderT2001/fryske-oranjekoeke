61878b8ca7bac20f9c94089f1133ebdecde88b88
Sander Tuinstra <sandert2001@hotmail.com>
2020-03-11 08:59:05 +0100

    HTML Default layout ident

--- app-skeleton/src/Views/Layouts/default.php
+++ app-skeleton/src/Views/Layouts/default.php
@@ -1,16 +1,16 @@
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title><?= APP_CONFIG['application']['name']; ?></title>
+    <head>
+        <meta charset="UTF-8">
+        <title><?= APP_CONFIG['application']['name']; ?></title>
 
-    <!-- Stylesheets -->
-    <!--<?= $this->Content->include('styles'); ?>-->
+        <!-- Stylesheets -->
+        <!--<?= $this->Content->include('styles'); ?>-->
 
-    <!-- Javascript -->
-    <!--<?= $this->Content->include('scripts'); ?>-->
-</head>
+        <!-- Javascript -->
+        <!--<?= $this->Content->include('scripts'); ?>-->
+    </head>
 
-<body>
-    <?= $this->Content->include('main'); ?>
-</body>
+    <body>
+        <?= $this->Content->include('main'); ?>
+    </body>
 </html>
